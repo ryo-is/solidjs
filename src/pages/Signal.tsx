0b2a7ad9dd97b77c653875fdc77ca1afc23b4920
ryo-is <ryosuke.izumi62@gmail.com>
2022-04-15 16:59:54 +0900

    feat: Add Buttons

--- src/pages/Signal.tsx
+++ src/pages/Signal.tsx
@@ -1,9 +1,26 @@
-import { createSignal, JSX } from 'solid-js';
+import { createSignal, Component } from 'solid-js';
 
-export const Signal = (): JSX.Element => {
+export const Signal: Component = () => {
   const [count, setCount] = createSignal(0);
 
-  setInterval(() => setCount(count() + 1), 1000);
+  const countUp = () => setCount((c) => c + 1);
+  const countDown = () => setCount((c) => c - 1);
 
-  return <div class="p-4">Connt: {count()}</div>;
+  return (
+    <div class="p-4">
+      <div class="">Connt: {count()}</div>
+      <button
+        class="bg-blue-600 hover:bg-blue-700 text-gray-200 py-2 px-4 rounded mt-2 mr-2"
+        onClick={countUp}
+      >
+        +1
+      </button>
+      <button
+        class="bg-red-600 hover:bg-red-700 text-gray-200 py-2 px-4 rounded mt-2"
+        onClick={countDown}
+      >
+        -1
+      </button>
+    </div>
+  );
 };
