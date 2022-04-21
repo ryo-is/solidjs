58f123bce23d707c5ad8383b0faf0234c89f8065
ryo-is <ryosuke.izumi62@gmail.com>
2022-04-21 11:26:26 +0900

    refactor

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -46,31 +46,29 @@ export const Store: Component = () => {
           Add Todo
         </button>
       </div>
-      <div class="form-check">
-        <For each={store.todos}>
-          {(todo) => {
-            const { id, text } = todo;
-            console.log(`Creating ${text}`);
-            return (
-              <div class="my-2 form-check">
-                <input
-                  type="checkbox"
-                  checked={todo.completed}
-                  onchange={[toggleTodo, id]}
-                  class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-                />
-                <label
-                  style={{
-                    'text-decoration': todo.completed ? 'line-through' : 'none',
-                  }}
-                >
-                  {text}
-                </label>
-              </div>
-            );
-          }}
-        </For>
-      </div>
+      <For each={store.todos}>
+        {(todo) => {
+          const { id, text } = todo;
+          console.log(`Creating ${text}`);
+          return (
+            <div class="my-2 form-check">
+              <input
+                type="checkbox"
+                checked={todo.completed}
+                onchange={[toggleTodo, id]}
+                class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
+              />
+              <label
+                style={{
+                  'text-decoration': todo.completed ? 'line-through' : 'none',
+                }}
+              >
+                {text}
+              </label>
+            </div>
+          );
+        }}
+      </For>
     </div>
   );
 };
